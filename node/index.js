0c5df9488d04e9d430fd3d5d6cc6784ca4733aca
Rca96 <raulcuritibamaral@gmail.com>
2024-04-25 10:25:02 -0300

    ajustes

--- node/index.js
+++ node/index.js
@@ -1,23 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
     database: 'nodedb'
-}
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+};
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('RaulTeste')`
-connection.query(sql)
-connection.end()
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS people (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255)
+    )
+`;
+connection.query(createTableQuery, (error, results, fields) => {
+    if (error) {
+        console.error('Erro ao criar tabela:', error);
+    } else {
+        console.log('Tabela criada com sucesso ou já existe.');
+    }
+});
+
+const insertDataQuery = `INSERT INTO people(name) values('RaulTeste')`;
+connection.query(insertDataQuery, (error, results, fields) => {
+    if (error) {
+        console.error('Erro ao inserir dados:', error);
+    } else {
+        console.log('Dados inseridos com sucesso.');
+    }
+});
+
+connection.end();
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks</h1>')
+    res.send('<h1>Full Cycle Rocks</h1>');
 });
 
 app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
-})
+    console.log('Rodando na porta ' + port);
+});
